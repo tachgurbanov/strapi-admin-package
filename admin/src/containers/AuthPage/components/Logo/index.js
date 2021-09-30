520634241270b4ac163152e148b1ac270dd98b3a
tachgurbanov <kerimtachgurbanow98@gmail.com>
2021-09-30 15:46:55 +0500

    first commit

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoStrapi from '../../../../assets/kt.jpeg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoStrapi} alt="strapi-logo" />;
+
+export default Logo;
